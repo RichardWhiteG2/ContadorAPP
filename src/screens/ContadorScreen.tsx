913bf341af027631c97e6f46ca8a316c9ff2426e
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-02-11 02:45:59 -0600

    55 StyleSheet

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,26 +1,43 @@
 import React, { useState } from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 
 export const ContadorScreen = () => {
     const [contador, setContador] = useState(10);
   return (
-    <View style={{
-        flex:1,
-        //backgroundColor: 'red',
-        justifyContent:'center'
-    }}>
+    <View style={styles.container}>
         <Text 
-            style={{
-                textAlign:'center', 
-                fontSize:40, 
-                position: 'relative',
-                top:-15
-            }}>
+            style={styles.title}>
                 Contador: {contador }</Text>
-        <Button
+        {/* <Button
             onPress={ ()=> setContador(contador+1)}
             title="Click"
-        />
+            /> */}
+        <TouchableOpacity
+            onPress={() => setContador(contador + 1)}
+        >
+            <View style={ styles.botonIncrementar}>
+                <Text>Click me !!</Text>
+            </View>
+        </TouchableOpacity>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+        container:{
+        flex:1,
+        //backgroundColor: 'red',
+        justifyContent:'center'
+    },
+    title:{
+        textAlign:'center', 
+        fontSize:40, 
+        position: 'relative',
+        top:-15
+    },
+    botonIncrementar:{
+        backgroundColor:'red',
+        borderRadius: 100
+    }
+})
